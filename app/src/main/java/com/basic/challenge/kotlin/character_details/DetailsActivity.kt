b9819b72015fca77567a6efaea23272a3e76bb23
Juan Diego <juandiegovillalobosquiros@gmail.com>
2018-10-17 01:29:10 +0200

    Errors fixed.

--- app/src/main/java/com/basic/challenge/kotlin/character_details/DetailsActivity.kt
+++ app/src/main/java/com/basic/challenge/kotlin/character_details/DetailsActivity.kt
@@ -1,7 +1,6 @@
 package com.basic.challenge.kotlin.character_details
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView
 import android.widget.TextView
