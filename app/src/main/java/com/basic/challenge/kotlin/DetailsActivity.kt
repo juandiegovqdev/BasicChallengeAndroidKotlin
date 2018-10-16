b56415c3a5ec6fa2bf29baf9632f7ccfb820ace5
juan <juandiegovillalobosquiros@gmail.com>
2018-10-16 22:57:23 +0200

    Some minor changes.

--- app/src/main/java/com/basic/challenge/kotlin/DetailsActivity.kt
+++ app/src/main/java/com/basic/challenge/kotlin/DetailsActivity.kt
@@ -19,12 +19,12 @@ class DetailsActivity : AppCompatActivity() {
         // Lines nª 17, 18 and 19 enable a button to go back
         val actionBar = actionBar
         actionBar!!.setHomeButtonEnabled(true)
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true)
 
         // Initialize UI Objects
-        var character_image : ImageView? = findViewById(R.id.character_image) as ImageView;
-        var character_name_text : TextView? = findViewById(R.id.character_name_text) as TextView;
-        var character_description_text : TextView? = findViewById(R.id.character_description_text) as TextView;
+        var character_image: ImageView? = findViewById(R.id.character_image) as ImageView
+        var character_name_text: TextView? = findViewById(R.id.character_name_text) as TextView
+        var character_description_text: TextView? = findViewById(R.id.character_description_text) as TextView
 
 
         //
